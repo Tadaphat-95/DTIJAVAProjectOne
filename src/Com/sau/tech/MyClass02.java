0154d0cfcae341a85d03c13627c4e949cc0558d5
Tadaphat-95 <s6752410028@sau.ac.th>
2025-01-14 17:00:50 +0700

    v.5: edit MyClass03 for User input 0-0

--- src/Com/sau/tech/MyClass02.java
+++ src/Com/sau/tech/MyClass02.java
@@ -2,6 +2,27 @@ package Com.sau.tech;
 
 public class MyClass02 {
     public static void main(String[] args) {
+        System.out.println("xxx");
+        System.out.println("yyy");
+        System.out.println("zzz");
+
+        System.out.println('M');
+//        System.out.println('');  error
+
+        System.out.println(1111);
+        System.out.println(123.443);
+
+        System.out.println(true);
+        System.out.println(false);
+
+        //Expreesion นิพจน์
+        System.out.println(10+20);
+
+        //ประสานข้อมูลแต่ละประเภทใช้ +
+        System.out.println(10 + 10 + "10"+ 10 );
+        System.out.println("10" + 10 + 10+ 10 );
+
+
 
     }
 }
--- src/Com/sau/tech/MyClass03.java
+++ src/Com/sau/tech/MyClass03.java
@@ -1,4 +1,25 @@
 package Com.sau.tech;
 
+import java.util.Scanner;
+
 public class MyClass03 {
+    public static void main(String[] args) {
+        String empname;
+        double empSalary , tax10;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input employee name: ");
+        empname = sc.next();
+        System.out.print("Input employee salary: ");
+        empSalary = sc.nextDouble();
+
+        System.out.println("----------------------");
+        tax10 = empSalary * 10 / 100;
+
+        System.out.println("Your name: " + empname);
+        System.out.println("Your salary: " + empSalary);
+        System.out.println("Tax pay: " + tax10 + "Baht");
+
+    }
 }
