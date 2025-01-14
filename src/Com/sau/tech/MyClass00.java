3bb66502b8fb81571cd889002f1d14fe293598bf
Tadaphat-95 <s6752410028@sau.ac.th>
2025-01-14 14:13:35 +0700

    v.2: create MyClass01 , MyClass02 and edit MyClass00

--- src/Com/sau/tech/MyClass00.java
+++ src/Com/sau/tech/MyClass00.java
@@ -3,5 +3,6 @@ package Com.sau.tech;
 public class MyClass00 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hi,World!");
     }
 }
---
+++ src/Com/sau/tech/MyClass01.java
@@ -0,0 +1,7 @@
+package Com.sau.tech;
+
+public class MyClass01 {
+    public static void main(String[] args) {
+
+    }
+}
